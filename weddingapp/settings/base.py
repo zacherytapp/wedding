a37baa4c5816a14427e85345b74c7af97394acee
zacherytapp <zacherytapp@gmail.com>
2015-08-09 22:42:24 -0400

    added aws support

--- weddingapp/settings/base.py
+++ weddingapp/settings/base.py
@@ -21,7 +21,6 @@ TEMPLATE_DEBUG = DEBUG
 ALLOWED_HOSTS = []
 
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -29,6 +28,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 )
 
 PROJECT_APPS = (
@@ -54,7 +54,6 @@ WSGI_APPLICATION = 'weddingapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -68,7 +67,6 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
-
 LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'  # 'Europe/London'
@@ -82,14 +80,28 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+AWS_STORAGE_BUCKET_NAME = 'zakk-and-kira-wedding'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID','')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY','')
 
-STATIC_URL = '/static/'
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s' % 's3-us-west-2.amazonaws.com/zakk-and-kira-wedding'
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
 
 MEDIA_ROOT = root('assets', 'uploads')
 MEDIA_URL = '/media/'
 
 # Additional locations of static files
-
 STATICFILES_DIRS = (
     root('assets'),
 )
@@ -98,8 +110,8 @@ TEMPLATE_DIRS = (
     root('templates'),
 )
 
-
-# .local.py overrides all the common settings.
+# Checks to see if this is a production instance - if
+# it is, it tells this document which sub-settings to pull from
 PRODUCTION_INSTANCE = os.environ['PRODUCTION_INSTANCE']
 if PRODUCTION_INSTANCE == True:
     try:
