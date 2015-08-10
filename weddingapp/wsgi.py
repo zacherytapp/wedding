b517edf2a6cfe034ad15a65fa052c966928d2772
zacherytapp <zacherytapp@gmail.com>
2015-08-09 23:54:32 -0400

    fixed wsgi

--- weddingapp/wsgi.py
+++ weddingapp/wsgi.py
@@ -13,8 +13,7 @@ from sys import path
 SITE_ROOT = dirname(dirname(abspath(__file__)))
 path.append(SITE_ROOT)
 
-
-os.environ.get("DJANGO_SETTINGS_MODULE", "weddingapp.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'weddingapp.settings')
 
 
 from django.core.wsgi import get_wsgi_application
