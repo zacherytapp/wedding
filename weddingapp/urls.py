6ed4bc5945d503295ae9ccb4d9dd91cf516f9113
zacherytapp <zacherytapp@gmail.com>
2015-12-07 19:27:20 -0500

    Updated ckeditor to latest version

--- weddingapp/urls.py
+++ weddingapp/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+
 from home.views import HomeView
 from contact.views import ContactCreate, ContactSuccessView
 
@@ -13,7 +14,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^ckeditor/', include('ckeditor.urls')),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    (r'^tinymce/', include('tinymce.urls')),
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^contact/add/$', ContactCreate.as_view()),
     url(r'^contact_success/$', ContactSuccessView.as_view())
